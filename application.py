1edb31d2b8e1fda36f4146a931152da742129a80
pronab1994 <pronab1712@gmail.com>
2025-12-29 12:03:51 -0500

    Fix Application Files

--- application.py
+++ application.py
@@ -118,4 +118,4 @@ def predict_datapoint():
         return render_template("home.html", results=None, error=str(e))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+    app.run(host="0.0.0.0", debug=True)
